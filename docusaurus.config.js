e39d950bced33f7bafa1f2ef29a2b6f1c3d20b71
ycm76229 <ycm76229@gmail.com>
2022-07-18 17:12:12 +0800

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,134 +6,146 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Notes Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://hjkl01.github.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+	title: 'My Notes Site',
+	tagline: 'Dinosaurs are cool',
+	url: 'https://hjkl01.github.io',
+	baseUrl: '/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'hjkl01', // Usually your GitHub org/user name.
-  projectName: 'hjkl01.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
-  trailingSlash: false,
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'hjkl01', // Usually your GitHub org/user name.
+	projectName: 'hjkl01.github.io', // Usually your repo name.
+	deploymentBranch: 'gh-pages',
+	trailingSlash: false,
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
 
-  themeConfig:
+	themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      navbar: {
-        title: 'My Notes Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Notes',
-          },
-          {to: '/docs/linux', label: 'Linux', position: 'left'},
-          {to: '/docs/docker', label: 'Docker', position: 'left'},
-          {to: '/docs/python', label: 'Python', position: 'left'},
+    	navbar: {
+    		title: 'My Notes Site',
+    		logo: {
+    			alt: 'My Site Logo',
+    			src: 'img/logo.svg',
+    		},
+    		items: [
+    			// {
+    			//   type: 'doc',
+    			//   docId: 'intro',
+    			//   position: 'left',
+    			//   label: 'Notes',
+    			// },
+    			{
+    				type: 'dropdown',
+    				label: 'Notes',
+    				position: 'left',
+    				items: [
+    					{to: '/docs/linux', label: 'Linux'},
+    					{to: '/docs/docker', label: 'Docker'},
+    					{to: '/docs/python', label: 'Python'},
+    					{to: '/docs/mac', label: 'Mac'},
+    				],
+    			},
 
-          {to: '/docs/bash-tutorial/intro', label: 'Bash Tutorial', position: 'right'},
-          {to: '/blog', label: 'Blog', position: 'right'},
-          {to: '/docs/tutorial/intro', label: 'Tutorial', position: 'right'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
+    			// {to: '/docs/linux', label: 'Linux', position: 'left'},
+    			// {to: '/docs/docker', label: 'Docker', position: 'left'},
+    			// {to: '/docs/python', label: 'Python', position: 'left'},
+
+    			{to: '/docs/bash-tutorial/intro', label: 'Bash Tutorial', position: 'right'},
+    			{to: '/blog', label: 'Blog', position: 'right'},
+    			{to: '/docs/tutorial/intro', label: 'Tutorial', position: 'right'},
+    			{
+    				href: 'https://github.com/facebook/docusaurus',
+    				label: 'GitHub',
+    				position: 'right',
+    			},
+    		],
+    	},
+    	footer: {
+    		style: 'dark',
+    		links: [
+    			{
+    				title: 'Docs',
+    				items: [
+    					{
+    						label: 'Tutorial',
+    						to: '/docs/intro',
+    					},
+    				],
+    			},
+    			{
+    				title: 'Community',
+    				items: [
+    					{
+    						label: 'Stack Overflow',
+    						href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+    					},
+    					{
+    						label: 'Discord',
+    						href: 'https://discordapp.com/invite/docusaurus',
+    					},
+    					{
+    						label: 'Twitter',
+    						href: 'https://twitter.com/docusaurus',
+    					},
+    				],
+    			},
+    			{
+    				title: 'More',
+    				items: [
+    					{
+    						label: 'Blog',
+    						to: '/blog',
+    					},
+    					{
+    						label: 'GitHub',
+    						href: 'https://github.com/facebook/docusaurus',
+    					},
+    				],
+    			},
+    		],
+    		copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    	},
+    	prism: {
+    		theme: lightCodeTheme,
+    		darkTheme: darkCodeTheme,
+    	},
     }),
 };
 
