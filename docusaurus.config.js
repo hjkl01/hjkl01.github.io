f575c5bb4f54880c6906393fb89b93d7f593551a
ycm76229 <ycm76229@gmail.com>
2022-07-20 14:46:12 +0800

    update add search bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,6 +55,25 @@ const config = {
 		],
 	],
 
+	themes: [
+		// ... Your other themes.
+		[
+			require.resolve('@easyops-cn/docusaurus-search-local'),
+			{
+				// ... Your options.
+				// `hashed` is recommended as long-term-cache of index file is possible.
+				hashed: true,
+				// For Docs using Chinese, The `language` is recommended to set to:
+				// ```
+				language: ["en", "zh"],
+				// ```
+                docsRouteBasePath: "/notes",
+                blogRouteBasePath: "/blog",
+                docsDir: "notes",
+			},
+		],
+	],
+
 	plugins: [
 
 		[
@@ -126,15 +145,11 @@ const config = {
     			{to: '/notes/docker', label: 'Docker', position: 'left'},
     			{to: '/notes/linux', label: 'Linux', position: 'left'},
     			{to: '/notes/python', label: 'Python', position: 'left'},
-
-    			// {to: '/docs/linux', label: 'Linux', position: 'left'},
-    			// {to: '/docs/docker', label: 'Docker', position: 'left'},
-    			// {to: '/docs/python', label: 'Python', position: 'left'},
+    			{to: '/notes/mac', label: 'Mac', position: 'left'},
 
     			{to: '/blog', label: 'Blog', position: 'right'},
-    			// {to: '/docs/intro', label: 'Tutorial', position: 'left'},
     			{
-    				href: 'https://github.com/facebook/docusaurus',
+    				href: 'https://github.com/hjkl01/hjkl01.github.io',
     				label: 'GitHub',
     				position: 'right',
     			},
@@ -142,48 +157,48 @@ const config = {
     	},
     	footer: {
     		style: 'dark',
-    		links: [
-    			{
-    				title: 'Docs',
-    				items: [
-    					{
-    						label: 'Tutorial',
-    						to: '/docs/intro',
-    					},
-    				],
-    			},
-    			{
-    				title: 'Community',
-    				items: [
-    					{
-    						label: 'Stack Overflow',
-    						href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-    					},
-    					{
-    						label: 'Discord',
-    						href: 'https://discordapp.com/invite/docusaurus',
-    					},
-    					{
-    						label: 'Twitter',
-    						href: 'https://twitter.com/docusaurus',
-    					},
-    				],
-    			},
-    			{
-    				title: 'More',
-    				items: [
-    					{
-    						label: 'Blog',
-    						to: '/blog',
-    					},
-    					{
-    						label: 'GitHub',
-    						href: 'https://github.com/facebook/docusaurus',
-    					},
-    				],
-    			},
-    		],
-    		copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    		// Links: [
+    		// 	{
+    		// 		title: 'Docs',
+    		// 		items: [
+    		// 			{
+    		// 				label: 'Tutorial',
+    		// 				to: '/docs/intro',
+    		// 			},
+    		// 		],
+    		// 	},
+    		// 	{
+    		// 		title: 'Community',
+    		// 		items: [
+    		// 			{
+    		// 				label: 'Stack Overflow',
+    		// 				href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+    		// 			},
+    		// 			{
+    		// 				label: 'Discord',
+    		// 				href: 'https://discordapp.com/invite/docusaurus',
+    		// 			},
+    		// 			{
+    		// 				label: 'Twitter',
+    		// 				href: 'https://twitter.com/docusaurus',
+    		// 			},
+    		// 		],
+    		// 	},
+    		// 	{
+    		// 		title: 'More',
+    		// 		items: [
+    		// 			{
+    		// 				label: 'Blog',
+    		// 				to: '/blog',
+    		// 			},
+    		// 			{
+    		// 				label: 'GitHub',
+    		// 				href: 'https://github.com/facebook/docusaurus',
+    		// 			},
+    		// 		],
+    		// 	},
+    		// ],
+    		copyright: `Copyright © ${new Date().getFullYear()} My Notes Project, Inc. Built with Docusaurus.`,
     	},
     	prism: {
     		theme: lightCodeTheme,
